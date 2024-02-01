8ca0a6c65db6647f30a5d6d181e175accd84e39c
Marwan Fikri <marwan.fikri200@gmail.com>
2024-02-01 12:45:02 +0100

    db tables

---
+++ database/migrations/2024_02_01_113641_create_all_tables.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('deadline');
+            $table->enum('type', ['design', 'marketing', 'development', 'testing', 'done']);
+            $table->enum('status', ['done', 'in_progress', 'todo']);
+            $table->timestamps();
+        });
+
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('deadline');
+            $table->timestamps();
+        });
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->foreignId('task_id');
+            $table->foreignId('user_id');
+            $table->timestamps();
+
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('comments');
+    }
+};
