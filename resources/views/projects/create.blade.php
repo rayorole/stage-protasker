417658e5d4173bdab9ebe837aa6a091cc4b47767
Ray Orole <orole.ray@protonmail.com>
2024-02-02 10:13:31 +0100

    Image preview

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -11,20 +11,37 @@
             @csrf
             <!-- Card -->
             <div class="bg-white rounded-xl shadow dark:bg-slate-900">
-                <div
+                <div id="output"
                     class="relative h-40 bg-[url('https://preline.co/assets/svg/examples/abstract-bg-1.svg')] bg-no-repeat bg-cover bg-center">
                     <div class="absolute top-0 end-0 p-4">
-                        <button type="button"
+
+
+                        <img id="output" />
+
+
+                        <label class="block">
+                            <span class="sr-only">Choose profile photo</span>
+                            <input type="file" onchange="loadFile(event)" accept="image/*" name="banner"
+                                class="block w-full text-sm text-gray-500
+                              file:me-4 file:py-2 file:px-4
+                              file:rounded-lg file:border-gray-100
+                              file:text-sm file:font-semibold
+                              file:bg-white file:text-gray-600
+                              hover:file:bg-gray-100
+                              file:disabled:opacity-50 file:disabled:pointer-events-none">
+                        </label>
+
+                        {{-- <input type="file"
                             class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium  border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-                            <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24"
-                                height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                                <polyline points="17 8 12 3 7 8" />
-                                <line x1="12" x2="12" y1="3" y2="15" />
-                            </svg>
-                            Upload banner
-                        </button>
+                        <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24"
+                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                            <polyline points="17 8 12 3 7 8" />
+                            <line x1="12" x2="12" y1="3" y2="15" />
+                        </svg>
+                        Upload banner
+                        </input> --}}
                     </div>
                 </div>
 
@@ -137,3 +154,16 @@
     </div>
     <!-- End Card Section -->
 </x-app-layout>
+
+
+<script>
+    const loadFile = function(event) {
+        const output = document.getElementById('output');
+        // Update bg-[url
+        output.style.backgroundImage = `url(${URL.createObjectURL(event.target.files[0])})`;
+
+        output.onload = function() {
+            URL.revokeObjectURL(output.src) // free memory
+        }
+    };
+</script>
